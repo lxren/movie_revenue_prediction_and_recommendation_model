319648383a804b57a309a17a38a6e111dac61d2c
Lily Ren <lilyxjren@gmail.com>
2024-04-17 01:16:07 -0600

    fixed the GDrive download and wrote the make_dataset.py for EDA

--- src/data/download_dataset.py
+++ src/data/download_dataset.py
@@ -1,15 +1,18 @@
 from urllib.request import urlretrieve
 
-urls = [
-    ("https://drive.google.com/file/d/1PWLqXQZzcBOcxNZVTRsro2H3xWjwzhHC/view?usp=drive_link",
-     "TMDB_movie_dataset_v11.csv"),
-    ("https://drive.google.com/file/d/15Nz4ldGeVGYK8znnPnhvtQL6GoL0hE2_/view?usp=drive_link",
-     "letterboxd_data.csv"),
-    ("https://drive.google.com/file/d/1DcyGoLsT8vvDwQX-tOge8YBlWURaCzfl/view?usp=drive_link",
-     "imdb.csv")
-]
+def download_dataset():
+    urls = [
+        ("1PWLqXQZzcBOcxNZVTRsro2H3xWjwzhHC",
+        "TMDB_movie_dataset_v11.csv"),
+        ("15Nz4ldGeVGYK8znnPnhvtQL6GoL0hE2_",
+        "letterboxd_data.csv"),
+        ("1DcyGoLsT8vvDwQX-tOge8YBlWURaCzfl",
+        "imdb.csv")
+    ]
 
-for (url, filename) in urls:
-    urlretrieve(url, f"data/external/{filename}")
+    for (fileId, filename) in urls:
+        urlretrieve(f"https://drive.usercontent.google.com/download?id={fileId}&export=download&authuser=1&confirm=t", f"data/raw/{filename}")
 
-
+if __name__ == '__main__':
+    download_dataset()
+ 
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,106 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas
 
+def make_dataset():
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    # Read csv and generate dataframes
+    letterboxd_data = pandas.read_csv("data/raw/letterboxd_data.csv")
+    imdb_data = pandas.read_csv("data/raw/imdb.csv")
+    tmdb_data = pandas.read_csv("data/raw/TMDB_movie_dataset_v11.csv")
+
+    # Drop duplicates
+    letterboxd_data = letterboxd_data.drop_duplicates(['imdb_id'])
+    letterboxd_data = letterboxd_data.drop_duplicates(['tmdb_id'])
+
+    # Letterboxd data drop and rename
+    columns_to_drop = ['_id', 'image_url', 'imdb_link', 'movie_id', 'tmdb_link', 'runtime', 'spoken_languages']
+    columns_to_rename = {'genres' : 'genre_letterboxd',
+                     'movie_title': 'title',
+                     'overview' : 'description_letterboxd',
+                     'year_released': 'year',
+                     'vote_average': 'rating_letterboxd',
+                     'vote_count': 'vote_count_letterboxd'}
+    if(any(column in columns_to_drop for column in letterboxd_data.columns)):
+        letterboxd_data.drop(columns_to_drop, axis=1, inplace=True)
+    else:
+        print("Column already dropped")
+
+    letterboxd_data.rename(columns=columns_to_rename, inplace=True)
+
+    # IMDB data drop and rename
+    columns_to_drop = ['Unnamed: 0', 'Unnamed: 1', 'certificate', 'year', 'movie_name', 'runtime']
+    columns_to_rename = {'movie_id' : 'imdb_id',
+                     'genre' : 'genre_imdb',
+                     'description' : 'description_imdb',
+                     'gross(in $)' : 'gross',
+                     'rating' : 'rating_imdb',
+                     'votes' : 'vote_count_imdb'}
+    if(any(column in columns_to_drop for column in imdb_data.columns)):
+        imdb_data.drop(columns_to_drop, axis=1, inplace=True)
+    else:
+        print("Column already dropped")
+
+    imdb_data.rename(columns=columns_to_rename, inplace=True)
+
+    # TMDB data drop and rename
+    columns_to_drop = ['vote_average',
+                   'vote_count',
+                   'status',
+                   'release_date',
+                   'backdrop_path',
+                   'homepage',
+                   'imdb_id',
+                   'original_language',
+                   'original_title',
+                   'popularity',
+                   'poster_path',
+                   'tagline',
+                   'production_countries',
+                   'title'
+                   ]
+    columns_to_rename = {'id' : 'tmdb_id',
+                      'overview' : 'description_tmdb',
+                      'genres' : 'genre_tmdb',
+                    }
+    if(any(column in columns_to_drop for column in tmdb_data.columns)):
+        tmdb_data.drop(columns_to_drop, axis=1, inplace=True)
+    else:
+        print("Column already dropped")
+    tmdb_data.rename(columns=columns_to_rename, inplace=True)
+
+    tmdb_data = tmdb_data.drop_duplicates(['tmdb_id'])
+
+    # Clean null values in Letterboxd
+    columns_to_exclude = ['original_language',
+                      'production_countries',
+                      'release_date',
+                      'spoken_languages',
+                      'vote_count'
+                      ]
+    letterboxd_data_clean = letterboxd_data.dropna(subset=[col for col in letterboxd_data.columns if col not in columns_to_exclude])
+
+    # Clean null values in IMDB
+    columns_to_exclude = ['runtime_imdb']
+    imdb_data_clean = imdb_data.dropna(subset=[col for col in imdb_data.columns if col not in columns_to_exclude])
+
+    # Clean null values in TMDB
+    columns_to_exclude = ['runtime_tmdb',
+                      'spoken_languages']
+    tmdb_data_clean = tmdb_data.dropna(subset=[col for col in tmdb_data.columns if col not in columns_to_exclude])
+
+    # Merge IMDB with Letterboxd
+    imdb_data_merged_letterboxd = pandas.merge(letterboxd_data_clean, imdb_data_clean, on=['imdb_id'], how='inner')
+
+    # Merge all three
+    tmdb_imdb_letterboxd_merged = pandas.merge(imdb_data_merged_letterboxd, tmdb_data_clean, on=['tmdb_id'], how='inner')
+
+    # Save to interim data
+    filepath = Path('data/interim/cleaned_film_dataset.csv')
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    tmdb_imdb_letterboxd_merged.to_csv(filepath, index=True)
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    make_dataset()
+ 
